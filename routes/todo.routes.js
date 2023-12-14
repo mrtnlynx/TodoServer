7563fbfe2a049978cf757d071aecf9284fdbce2c
mrtnlynx <martinilves@mail.ru>
2023-12-14 21:04:22 +0200

    add files

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,50 +1,48 @@
 const express = require('express');
-
+const db = require('../db.js');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-
     try {
-        const data = db.query('SELECT * FROM todo;');
-        res.status(200).json({todo: data.rows});
-    }
-    catch(error) {
+        const data = await db.query('SELECT * FROM todo;');
+        console.log('Got the following data:', data.rows);
+        res.status(200).json({ todo: data.rows });
+    } catch (error) {
         console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-    
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post('/', async (req, res) => {
+    console.log('Received POST request on /todos with body:', req.body);
     const { task } = req.body;
-
     try {
         const data = await db.query('INSERT INTO todo (task) VALUES ($1);', [task]);
-        console.log(data);
-        res.status(200).json({message: `${data.rowCount} row inserted.`});
-    } 
-    catch (error) {
-        console.log(error);
-    } 
-
-});
-
-router.delete('/', async (req, res) => {
-    const {id} = req;
-    const data = await db.query("SELECT * FROM todo WHERE id = $1;", [id]);
-
-    if(data.rows.length === 0) {
-        res.json({message: "there no such task"});
-    } else {
-        try {
-            const result = await db.query("DELETE FROM todo WHERE id = $1;", [id]);
-            res.status(200).json({message: `${result.rowCount} row was deleted.`});
-        }
-        catch(error) {
-            console.log(error);
-        }
+        console.log('Insert operation result:', data);
+        res.status(200).json({ message: `${data.rowCount} row inserted.` });
+    } catch (error) {
+        console.error('Error in POST /todos:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
 
-module.exports = ; 
+router.delete('/', async (req, res) => {
+    const { id } = req.body;
+    console.log('Received DELETE request on /todos for id:', id);
+    try {
+        const data = await db.query('SELECT * FROM todo WHERE id = $1;', [id]);
+        if (data.rows.length === 0) {
+            res.json({ message: 'No task found with the specified ID.' });
+        } else {
+            const result = await db.query('DELETE FROM todo WHERE id = $1;', [id]);
+            console.log('Delete operation result:', result);
+            res.status(200).json({ message: `${result.rowCount} row was deleted.` });
+        }
+    } catch (error) {
+        console.error('Error in DELETE /todos:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const todoRouter = require();
+const todoRouter = require('./routes/todo.routes');
 const dotenv = require('dotenv');
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/todos', todoRouter);
+const PORT = process.env.PORT || 3000;
 
-app.use('/', );
-
-app.listen(, () => {
+app.listen(PORT, () => {
     console.log('Server is running on Port 3000.');
 });
